5b73d17a2da286f75a99f5a32a717426d0741376
Peter Rossau von Hedemann <Peter.Hedemann@UFST.DK>
2018-10-25 14:14:46 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task("dev:styles:sass", () => {
+    return gulp.src("src/sass/skat.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("dev/css/"));
+});
+
+gulp.task("dev:styles:guide", () => {
+    return gulp.src("src/guide-assets/css/guide.css")
+        .pipe(gulp.dest("dev/css"));
+});
+
+gulp.task("dev:images", () => {
+    return gulp.src("src/images/**/*")
+        .pipe(gulp.dest("dev/images"));
+});
+
+gulp.task("dev:styles", ["dev:styles:sass", "dev:styles:guide"]);
+
+gulp.task("dev:scripts", () => {
+    return gulp.src([
+        "src/guide-assets/js/*.js", 
+        "node_modules/jquery/dist/jquery.js"])
+        .pipe(gulp.dest("dev/js"));
+});
+
+gulp.task("dev:watch", ["dev:styles", "dev:scripts", "dev:images"], () => {
+    gulp.watch("src/sass/**/*.scss", ["dev:styles:sass"]);
+    gulp.watch("src/guide-assets/**/*.js", ["dev:scripts"]);
+    gulp.watch("src/guide-assets/css/guide.css", ["dev:styles:guide"]);
+});
---
+++ quickguide.md
@@ -0,0 +1,8 @@
+# To get up and running,
+
+1. install Ruby, bundler and Jekyll (see also: https://jekyllrb.com/docs/installation/)
+2. clone project https://peterhedemann@bitbucket.org/peterhedemann/bootskat4.git
+3. run **npm install**
+4. run **bundle install**
+5. run **npm run watch**
+6. Open **\_site/udvikling_elementer.html** in a browser
